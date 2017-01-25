4c1ee10fba9d70c916d460a98f5824574ddad1b3
Rob Anderson <robert.anderson2@ncl.ac.uk>
2017-01-25 13:09:42 +0000

    + Added cronjobs tweak + Changed test script to set dev env

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,10 @@
 php _check_env.php DB_HOST DB_USER DB_PASS DB_NAME
 
 
+# copy env vars for cronjobs
+env | sed 's/^\(.\)/export \1/' >> /app/.cron_env
+
+
 # Build the database
 framework/sake dev/build "flush=1"
 
