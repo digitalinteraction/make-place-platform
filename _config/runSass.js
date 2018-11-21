903877dff1a14bbd7dbf94394e8e5b73642dce9e
Rob Anderson <rob.anderson@ncl.ac.uk>
2018-11-21 13:43:38 +0000

    :star: Only regenerate sass if variables change

--- _config/runSass.js
+++ _config/runSass.js
@@ -1,6 +1,9 @@
-const path = require('path')
+#!/usr/bin/env node
+
+const { join } = require('path')
+const { readFileSync, writeFileSync } = require('fs')
+const crypto = require('crypto')
 const webpack = require('webpack')
-const ora = require('ora')
 const chalk = require('chalk')
 const rm = require('rimraf')
 
@@ -8,14 +11,55 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 process.env.NODE_ENV = 'production'
-
 const config = require('./webpack/webpack.prod.config')
 
-let spinner = ora('Compiling Sass ...')
-spinner.start()
+const varsHashPath = join(__dirname, '../assets/vars.hash')
+const readVarsHash = () => readFileSync(varsHashPath, 'utf8')
+const readVars = () => readFileSync(join(__dirname, 'scss/vars.scss'), 'utf8')
+
+// Gets a hash of the current sass variables
+function hashCurrentVars() {
+  return crypto
+    .createHash('sha256')
+    .update(readVars())
+    .digest('hex')
+}
+
+// Only regenerate if the sass variables have changed
+function skipRegeneration() {
+  try {
+    return hashCurrentVars() === readVarsHash()
+  }
+  catch (err) {
+    return false
+  }
+}
+
+// Write the hash of css variables to a file in a volume
+function writeVarsHash() {
+  try {
+    writeFileSync(varsHashPath, hashCurrentVars())
+  }
+  catch (err) {
+    console.log('Failed', err)
+    process.exit(1)
+  }
+}
+
+// Exit now if we should skip sass regeneration
+if (skipRegeneration()) {
+  console.log('Skipping sass regeneration')
+  process.exit(0)
+}
+
+//
+// Regenerate css assets based on new variables
+//
+
+console.log('Regenerating css with changed variables')
 
 // Point to a different directory
-const newPath = path.join(__dirname, '../public/_tmp')
+const newPath = join(__dirname, '../public/_tmp')
 config.output.path = newPath
 
 // Only use the theme input
@@ -29,24 +73,25 @@ config.plugins = [
   })
 ]
 
-
 // Run webpack
 webpack(config).run((err, result) => {
-  
-  spinner.stop()
   if (err) throw err
+
+  process.stdout.write(
+    result.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+      hash: false,
+      version: false
+    }) + '\n\n'
+  )
+
+  // Remove the temporary dir
+  rm(newPath, () => chalk.cyan('  Build complete.\n'))
   
-  process.stdout.write(result.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false,
-    hash: false,
-    version: false
-  }) + '\n\n')
-  
-  rm(newPath, () => {
-    chalk.cyan('  Build complete.\n')
-  })
+  // Write the variables hash
+  writeVarsHash()
 })
